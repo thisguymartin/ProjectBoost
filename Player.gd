df7427dab73310efac6cb86b0448b5418860a9d5
Martin Patino <mpatino117@gmail.com>
2024-02-23 10:26:53 -0700

    chore: add script

--- Player.gd
+++ Player.gd
@@ -10,6 +10,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		apply_central_force(Vector3.UP * delta * 1000)
-	pass
+
+	if Input.is_action_pressed("ui_accept"):
+		apply_central_force(Vector3.UP * delta * 1000.0)
+
