3ba2297292b31f1626233b00e5bc482a306d3274
Martin Patino <mpatino117@gmail.com>
2024-02-23 10:07:26 -0700

    chore: add script

--- Player.gd
+++ Player.gd
@@ -1,4 +1,4 @@
-extends Node3D
+extends RigidBody3D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,5 +11,5 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
-		print("action here")
+		apply_central_force(Vector3.UP * delta * 1000)
 	pass
