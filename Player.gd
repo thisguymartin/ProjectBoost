fccd6e2d55633925767b417a8ff8da9f7364ff2f
Martin Patino <mpatino117@gmail.com>
2024-02-23 10:43:57 -0700

    chore: update player

--- Player.gd
+++ Player.gd
@@ -12,5 +12,13 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 
 	if Input.is_action_pressed("ui_accept"):
-		apply_central_force(Vector3.UP * delta * 1000.0)
-
+		apply_central_force(basis.y * delta * 100.0)
+	
+	if Input.is_action_pressed("ui_left"):
+		apply_torque(Vector3(0,0,50 * delta))
+	
+	if Input.is_action_pressed("ui_right"):
+		apply_torque(Vector3(0,0,-50 * delta))
+	
+	if Input.is_action_pressed("ui_down"):
+		apply_central_force(basis.y * delta * -100.0)
