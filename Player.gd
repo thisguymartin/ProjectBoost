47c9b7add139a1cf5729e65ffd7394e1e2f2179f
Martin Patino <mpatino117@gmail.com>
2024-02-26 17:01:36 -0700

    chore: update godot

--- Player.gd
+++ Player.gd
@@ -11,14 +11,29 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 
-	if Input.is_action_pressed("ui_accept"):
+	if Input.is_action_pressed("boost"):
 		apply_central_force(basis.y * delta * 1000.0)
 	
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("turn_right"):
 		apply_torque(Vector3(0,0,100 * delta))
 	
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("turn_left"):
 		apply_torque(Vector3(0,0,-100 * delta))
 	
 	if Input.is_action_pressed("ui_down"):
 		apply_central_force(basis.y * delta * -1000.0)
+
+
+func _on_body_entered(body: Node)-> void:
+	if "Goal" in body.get_groups():
+		print("You win")
+		
+	if "Failed" in body.get_groups():
+		print("You loose")
+		
+		
+	
+		
+	
+		
+
